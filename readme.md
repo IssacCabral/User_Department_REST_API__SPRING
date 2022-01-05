c3dc0517dc023cf3fe0cdb3253c8145e72289bb7
Clidenor Issac <issaccabrall@gmail.com>
2022-01-05 13:56:25 -0300

    Readme Adicionado

---
+++ readme.md
@@ -0,0 +1,15 @@
+Aqui nós temos uma simples API REST que responde à requisições
+de Usuários e departamentos
+
+Tecnologias Usadas:
+* Spring Boot
+* ORM: Spring Data JPA
+* H2 -> como banco em memória
+
+<h2>Listando os usuários</h2>
+<img src="./preview/1.png">
+<h2>Mostrando por id</h2>
+<img src="./preview/2.png">
+<h2>Enviando um novo usuário na requisição</h2>
+<img src="./preview/3.png">
+<img src="./preview/4.png">
--- src/main/java/com/issaccabral/userdept/controllers/UserController.java
+++ src/main/java/com/issaccabral/userdept/controllers/UserController.java
@@ -1,15 +1,34 @@
 package com.issaccabral.userdept.controllers;
 
 import com.issaccabral.userdept.entities.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.issaccabral.userdept.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
     public List<User> findAll(){
-        
+        List<User> result = repository.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public User findById(@PathVariable Long id){
+        User user = repository.findById(id).get();
+        return user;
+    }
+
+    @PostMapping
+    public User insert(@RequestBody User user){
+        User result = repository.save(user);
+        return result;
     }
 }
